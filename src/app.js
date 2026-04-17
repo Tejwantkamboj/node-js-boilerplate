ad022fdf182cbe07ecf857f20746f88bac51e882
Tejwant <tejwant2444@yopmail.com>
2026-04-17 21:09:05 +0530

    frontend origin

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://react-tanstack-boilerplate.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
